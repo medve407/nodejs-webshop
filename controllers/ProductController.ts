485542ebb70d7df0e6bfe0f5b84743cf094350e2
david <I504105@mvrht.com>
2017-03-02 11:49:43 +0100

    Created a controller for products

--- controllers/ProductController.ts
+++ controllers/ProductController.ts
@@ -1,11 +1,20 @@
 let BaseController = require('../Framewoork/Controller/BaseController');
 let Product 	   = require('../models/Product');
 
-abstract class ProductController extends BaseController {
+class ProductController extends BaseController {
 
-	static sayHello(req,res):void{
-		res.send("2222");
+	// It return the index of the product with Products if they are exists.
+	public static listProducts(req,res):any {
+		res.render('product/index',{
+			'products' : Product.all()
+		});
+	}
+	// It return information about a project by an id from parameter.
+	public static getInfo(req,res) {
+		res.render('product/detail',{
+			'product': Product.all()[0]
+		});
 	}
 }
 
-module.exports = ProductController;
+export = ProductController;
