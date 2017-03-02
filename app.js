0630c6a42bc2ce93564933c4b706a8c8f7ebcf31
david <I504105@mvrht.com>
2017-03-02 11:56:30 +0100

    Added some session options, and route options as well.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express   = require('express');
+var path      = require('path');
+var favicon   = require('serve-favicon');
+var logger    = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var session = require('express-session');
 
-var index = require('./routes/index');
+var index   = require('./routes/index');
 var Product = require('./routes/Product');
+var Cart    = require('./routes/Cart');
 
 var app = express();
 
@@ -20,6 +22,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'Iamverysecret',
+  // It false because we dont want save each request every session, no matter something changed or not.
+  resave: false,
+  // If true, it store the session in the server even so if nothing has changed.
+  saveUnitialized: false
+}));
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -30,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/product', Product);
+app.use('/cart', Cart);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
